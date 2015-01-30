84d4c846e527e92a0166b5c5751052c36936b311
Daniel Passos <daniel@passos.me>
2015-01-30 16:30:06 -0200

    Remove unnecessary menu

--- app/src/main/java/org/abstractj/goose/MainActivity.java
+++ app/src/main/java/org/abstractj/goose/MainActivity.java
@@ -3,15 +3,12 @@ package org.abstractj.goose;
 import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import java.io.IOException;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
-
 
 public class MainActivity extends ActionBarActivity {
 
@@ -41,29 +38,6 @@ public class MainActivity extends ActionBarActivity {
         // Tell the URLConnection to use a SocketFactory from our SSLContext
         new HttpRequestTask(assetManager.open("random.org.pem")).execute();
 
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
 }
